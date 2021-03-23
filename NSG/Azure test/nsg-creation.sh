1a944cb75f189ce8a6088afe2fcc1921a8af37c7
Luis Gutierrez <luisgutierrez707@gmail.com>
2021-03-22 18:21:23 -0700

    c
    c

--- NSG/Azure test/nsg-creation.sh
+++ NSG/Azure test/nsg-creation.sh
@@ -1,1 +1,4 @@
 az network nsg rule create --name Work --priority 200 --access Allow --direction Inbound --source-port-range 10 --resource-group nsg-testing-scripts --nsg-name nsg-should-not-work
+
+
+az deployment create --location WestUS --template-file azuredeploy.jsonls
